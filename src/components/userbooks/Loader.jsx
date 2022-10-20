db66cfa7b4833ff706cad52eec3e24a5139ca0c6
Azima Sati <106227073+sati224@users.noreply.github.com>
2022-10-20 18:55:13 +0400

    added DropDown and Loader (#6)

---
+++ src/components/userbooks/Loader.jsx
@@ -0,0 +1,10 @@
+import loading from "../images/loading2.gif";
+const Loader = () => {
+  return (
+    <article className="loading">
+      <img src={loading} alt="loading" />
+    </article>
+  );
+};
+
+export default Loader;
